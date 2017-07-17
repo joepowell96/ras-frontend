2628a25a5fd40908d1e90f255fb2d58c3bae9582
Ramin <ramin.esfan@gmail.com>
2017-07-17 15:24:59 +0100

    RAS-392: Create find member details page

--- test/controllers/MemberDetailsControllerSpec.scala
+++ test/controllers/MemberDetailsControllerSpec.scala
@@ -16,8 +16,9 @@
 
 package controllers
 
+import play.api.http.Status
 import play.api.test.FakeRequest
-import play.api.test.Helpers._
+import play.api.test.Helpers.{contentType, _}
 import uk.gov.hmrc.play.test.{UnitSpec, WithFakeApplication}
 
 class MemberDetailsControllerSpec extends UnitSpec with WithFakeApplication {
@@ -33,5 +34,15 @@ class MemberDetailsControllerSpec extends UnitSpec with WithFakeApplication {
       status(result.get) should not equal (NOT_FOUND)
     }
 
+    "return 200" in {
+      val result = TestMemberDetailsController.get(fakeRequest)
+      status(result) shouldBe Status.OK
+    }
+
+    "return HTML" in {
+      val result = TestMemberDetailsController.get(fakeRequest)
+      contentType(result) shouldBe Some("text/html")
+      charset(result) shouldBe Some("utf-8")
+    }
   }
 }
